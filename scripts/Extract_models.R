7d23fe14547b4a6b326a41fc5f370e4259f8fbe4
jesperfischer <jesperfischer@outlook.dk>
2026-01-25 18:20:29 +0100

    updates

--- scripts/Extract_models.R
+++ scripts/Extract_models.R
@@ -1,13 +1,13 @@
 get_models <- function(osftoken){
   # get the preanalyzed data from osf:
-  if (!dir.exists(here::here("results"))) {
-    osfr::osf_auth(token = osf_token)
+  if (dir.exists(here::here("results"))) {
+    osfr::osf_auth(token = osftoken)
     
     HI <- osfr::osf_retrieve_node("https://osf.io/8yhtp/")
     
     downloaded = HI %>%
-      osfr::osf_ls_files(pattern = "results") %>%
-      osfr::osf_download(path = here::here(""), recurse = TRUE, conflicts = "overwrite", progress = TRUE)
+      osfr::osf_ls_files(pattern = "fit population") %>%
+      osfr::osf_download(path = here::here("results"), recurse = TRUE, conflicts = "overwrite", progress = TRUE)
     
     
     
@@ -15,6 +15,7 @@ get_models <- function(osftoken){
   
 }
 
+get_models(osf_token)
 
 osf_file_path <- here::here("osf","osf.txt")
 osf_token <- read_lines(osf_file_path)[1]
